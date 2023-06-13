fb1324346ff37dd5385a740875410400e4a74837
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-06-13 10:38:23 -0700

    Attempt to Fix image linking issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from "react"; 
+import email from "../assets/email.png"
 
 export default function Footer(){
     return(
 <div className = "footer">
-    <a href="https://www.linkedin.com/in/shelby-francisco-111948100/"><img src="/linkedin.png" alt ="linkedin logo" width="30px"></img></a>
-    <a href="mailto:shelbyfrancisco@hotmail.com"><img src="/email.png" alt ="email logo" width="30px"></img></a>
+    <a href="https://www.linkedin.com/in/shelby-francisco-111948100/"><img src="./SFran_React_Portfolio/linkedin.png" alt ="linkedin logo" width="30px"></img></a>
+    <a href="mailto:shelbyfrancisco@hotmail.com"><img src={email} alt ="email logo" width="30px"></img></a>
     <a href="https://github.com/Shelcisco"><img src="/github.png" alt ="github logo" width="35px"></img></a>
    </div>
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -32,7 +32,7 @@ const renderPage = ()=>{
         return (
             <div>
                 <h1>About Me</h1>
-                <p> <img src="/Mee.jpg" height="100" alt="" id = "me"></img> Hi there, my name is Shelby. After working in financials for years I wanted to try something more creative than crunching numbers, I took a big step and signed up for the UW's coding bootcamp.</p> 
+                <p> <img src="/SFran_React_Portfolio/Mee.jpg" height="100" alt="" id = "me"></img> Hi there, my name is Shelby. After working in financials for years I wanted to try something more creative than crunching numbers, I took a big step and signed up for the UW's coding bootcamp.</p> 
  <p>While there's nothing wrong with crunching, I've found I prefer coding! I'm consistently working to improve my skills, please see my portfolio to look at what I've built so far. Thank you!  </p>
             </div>
         )
