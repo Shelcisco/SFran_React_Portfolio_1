01c852393db6239e180b28f48b111f539548fa3b
Shelcisco <119394650+Shelcisco@users.noreply.github.com>
2023-06-12 17:45:07 -0700

    ADD Content Wrapper / IMPORT CSS

--- TEST/portfolio/src/App.js
+++ TEST/portfolio/src/App.js
@@ -2,13 +2,16 @@ import React, {useState} from "react";
 import Main from './components/Main';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import "./index.css" 
 
 function App() {
   const [page,setPage] = useState("about");
   return (
     <div className="App">
+      <div className="content-wrapper">
       <Header page={page} setPage={setPage}/>
       <Main page={page}/>
+      </div>
   <Footer/>
     </div>
   );
